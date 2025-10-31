c68734b907fa2328683234c1993e8895215abb21
SkodingTech <info@skoding.com>
2025-10-31 22:19:32 +0530

    V.1.12

--- components/blog/BlogCard1.js
+++ components/blog/BlogCard1.js
@@ -2,9 +2,14 @@ import Link from "next/link"
 import Slugify from "@/components/validators/slugify";
 import { useState } from "react";
 // import Head from "next/head"
+import parse from 'html-react-parser';
 
 export default function BlogCard1({ item }) {
-    const [slugnName, SlugName] = useState(item?.title.rendered ? Slugify(item?.title.rendered) : null)
+
+    const cleanContent = (item?.title.rendered || "").replace(/\[\+\d+\s+chars\]/g, "");
+    const detailData = parse(cleanContent);
+
+    const [slugnName, SlugName] = useState(cleanContent);
 
     const formatDate = (dateString) => {
         const date = new Date(dateString)
@@ -17,38 +22,13 @@ export default function BlogCard1({ item }) {
     console.log(item);
     return (
         <>
-            {/* <Head>
-                <title>{item.yoast_head_json.title ? item.yoast_head_json.title : ""}</title>
-                <meta name="title" content={item.yoast_head_json.title ? item.yoast_head_json.title : ""}></meta>
-                <meta name="description" content={item.yoast_head_json.description ? item.yoast_head_json.description : ""}></meta>
-            </Head> */}
-            {/* <div className="inner-blog-item">
-                <div className="inner-blog-thumb">
-                    <Link href={`/blog/${item.id}`}><img src={`/assets/img/blog/${item.img}`} alt="" /></Link>
-                </div>
-                <div className="inner-blog-content">
-                    <div className="blog-meta-two">
-                        <ul className="list-wrap">
-                            <li className="tag"><Link href={`/blog/${item.id}`}>Sector</Link></li>
-                            <li><i className="fal fa-clock" />5 Min</li>
-                            <li><i className="fal fa-calendar" />{item.date}</li>
-                            <li>By <Link href={`/blog/${item.id}`}>{item.author}</Link></li>
-                        </ul>
-                    </div>
-                    <h2 className="title"><Link href={`/blog/${item.id}`}>{item.title}</Link></h2>
-                    <p>Lorem ipsum dolor sit amet, sed nulla ante amet, elementum tincidunt arcu sed laoreet, natoque ac eget imperdiet. Ac scelerisque nibh dolores consectetuer, nulla aptent est pede. Scelerisque euismod varius mi, congue eget sed vestibulum, ornare cras sed nec.</p>
-                    <Link href={`/blog/${item.id}`} className="rade-more-btn">Read More</Link>
-                </div>
-            </div> */}
-
             <div className="col-lg-6 col-md-6">
                 <div className="blog-post-item">
                     <div className="blog-post-thumb">
-                        <Link href={`/blog/${slugnName}/${item.id}`}><img
+                        <Link href={`/blog/${Slugify(slugnName)}/${item.id}`}><img
                             src={`${item?.yoast_head_json?.og_image ? item?.yoast_head_json?.og_image[0].url : null}`}
                             alt=""
-                            style={{ width: "100%", height: "300px", objectFit: "cover" }}
-                        />
+                            style={{ width: "100%", height: "300px", objectFit: "cover" }} />
                         </Link>
                     </div>
                     <div className="blog-post-content">
@@ -59,7 +39,7 @@ export default function BlogCard1({ item }) {
                                 <li><i className="fas fa-calendar-alt" />{formatDate(item.date)}</li>
                             </ul>
                         </div>
-                        <h2 className="title"><Link href={`/blog/${slugnName}/${item.id}`}>{item.title.rendered ? item.title.rendered.substring(0, 55) : null}</Link>
+                        <h2 className="title"><Link href={`/blog/${Slugify(slugnName)}/${item.id}`}>{detailData ? detailData.substring(0, 55) : null}</Link>
                         </h2>
                         <Link href={`/blog/${slugnName}/${item.id}`} className="link-btn">Read More<i className="fas fa-arrow-right" /></Link>
                     </div>
--- pages/blog.js
+++ pages/blog.js
@@ -6,6 +6,7 @@ import { baseURL } from "@/auth/auth";
 import { useRouter } from "next/navigation";
 import Slugify from "@/components/validators/slugify";
 import Head from "next/head"
+import parse from 'html-react-parser';
 
 export default function Blog() {
 
@@ -66,6 +67,7 @@ export default function Blog() {
             <Layout breadcrumbTitle="Blog">
                 <section className="pt-30 pb-120">
                     <div className="container">
+
                         <div className="row justify-content-center">
                             <div className="col-lg-6">
                                 <div className="section-title text-center mb-50 tg-heading-subheading animation-style3">
@@ -83,33 +85,12 @@ export default function Blog() {
                             </div>
                             <div className="col-xl-4 col-lg-6 col-md-10">
                                 <aside className="blog-sidebar">
-                                    {/* <div className="blog-widget">
-                                        <div className="sidebar-search">
-                                            <h4 className="widget-title">Search</h4>
-                                            <form action="#">
-                                                <input id="search" type="text" placeholder="Search" />
-                                                <button type="submit"><i className="fas fa-search" /></button>
-                                            </form>
-                                        </div>
-                                    </div> */}
-                                    {/* <div className="blog-widget">
-                                        <h4 className="widget-title">Categories</h4>
-                                        <div className="categories-list">
-                                            <ul className="list-wrap">
-                                                <li><Link href="/#">Business Consulting <span>(05)</span></Link></li>
-                                                <li><Link href="/#">Financial Investment <span>(07)</span></Link></li>
-                                                <li><Link href="/#">IT Consultations <span>(03)</span></Link></li>
-                                                <li><Link href="/#">Digital Analytics <span>(04)</span></Link></li>
-                                                <li><Link href="/#">Payment Processing <span>(02)</span></Link></li>
-                                                <li><Link href="/#">Software Solutions <span>(09)</span></Link></li>
-                                            </ul>
-                                        </div>
-                                    </div> */}
                                     <div className="blog-widget">
                                         <h4 className="widget-title">Recent News</h4>
                                         <div className="rc-post-wrap">
                                             {blogsData.map((data, i) => {
                                                 let slugnName = data?.title.rendered ? Slugify(data?.title.rendered) : null;
+                                                const cleanContent = (data?.title.rendered || "").replace(/\[\+\d+\s+chars\]/g, "");
                                                 return (
                                                     <>
                                                         <div className="rc-post-item">
@@ -118,57 +99,23 @@ export default function Blog() {
                                                                     style={{ width: "100%", height: "90px", objectFit: "cover" }} alt={data?.title.rendered ? Slugify(data?.title.rendered) : ""} /></Link>
                                                             </div>
                                                             <div className="rc-post-content">
-                                                                <h5 className="title"><Link href={`/blog/${slugnName}/${data.id}`}>{data?.title.rendered ? data?.title.rendered : ""}</Link></h5>
+                                                                <h5 className="title"><Link href={`/blog/${slugnName}/${data.id}`}>{parse(cleanContent)}</Link></h5>
                                                                 <span><i className="fas fa-calendar-alt" />{formatDate(data.date)}</span>
                                                             </div>
                                                         </div>
                                                     </>
                                                 )
                                             })}
-
-                                            {/* <div className="rc-post-item">
-                                                <div className="rc-post-thumb">
-                                                    <Link href="/blog-details"><img src="/assets/img/blog/rc_post02.jpg" alt="" /></Link>
-                                                </div>
-                                                <div className="rc-post-content">
-                                                    <h5 className="title"><Link href="/blog-details">Medium shot men working on roof presentation</Link></h5>
-                                                    <span><i className="fas fa-calendar-alt" />22 Jan {new Date().getFullYear()}</span>
-                                                </div>
-                                            </div>
-                                            <div className="rc-post-item">
-                                                <div className="rc-post-thumb">
-                                                    <Link href="/blog-details"><img src="/assets/img/blog/rc_post03.jpg" alt="" /></Link>
-                                                </div>
-                                                <div className="rc-post-content">
-                                                    <h5 className="title"><Link href="/blog-details">Free photo cute family a roof above their head</Link></h5>
-                                                    <span><i className="fas fa-calendar-alt" />22 Jan {new Date().getFullYear()}</span>
-                                                </div>
-                                            </div> */}
                                         </div>
                                     </div>
-                                    {/* <div className="blog-widget">
-                                        <h4 className="widget-title">Popular Tags</h4>
-                                        <div className="tag-list-wrap">
-                                            <ul className="list-wrap">
-                                                <li><Link href="/#">Construction</Link></li>
-                                                <li><Link href="/#">Roofing</Link></li>
-                                                <li><Link href="/#">Repairer</Link></li>
-                                                <li><Link href="/#">Maintenance</Link></li>
-                                                <li><Link href="/#">Remodeling</Link></li>
-                                                <li><Link href="/#">Roof</Link></li>
-                                                <li><Link href="/#">Roofing Service</Link></li>
-                                                <li><Link href="/#">Consulting</Link></li>
-                                            </ul>
-                                        </div>
-                                    </div> */}
-                                    <div className="blog-widget widget-bg" data-background="/assets/img/blog/widget_bg.jpg">
+                                    {/* <div className="blog-widget widget-bg" data-background="/assets/img/blog/widget_bg.jpg">
                                         <h4 className="widget-title">Need Help</h4>
                                         <div className="sidebar-content">
                                             <h4 className="title">Beautiful red brick house with decorative lights</h4>
                                             <p>Sit amet consectetur adipiscing elseds do eius mod tempor incididunt</p>
                                             <Link href="/contact" className="btn btn-two">Contact Us</Link>
                                         </div>
-                                    </div>
+                                    </div> */}
                                 </aside>
                             </div>
                         </div>
